fc62d468acb445b54339758ac6b09cbf2b88a2db
Ranajoy Dutta <ranajoydutta7@gmail.com>
2021-11-09 23:56:18 +0530

    InProgress:Registration,Login w-token verification

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def register(request):
+    return render(request, "accounts/register.html")
+
+
+def login(request):
+    return render(request,"accounts/signin.html")
+
+
+def logout(request):
+    return HttpResponse("logout page")
